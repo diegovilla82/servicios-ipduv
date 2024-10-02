58892b501f1461393e8274f1b3f30ed92b2cd893
VILLARROEL Diego (diego.villarroel) <diego.villarroel@chaco.gob.ar>
2024-10-02 07:24:29 -0300

    change logo

--- resources/views/report/pdf.blade.php
+++ resources/views/report/pdf.blade.php
@@ -28,7 +28,7 @@
 <title>Nota solicitud de Inscripción al Registro de Personas Físicas (Instaladores Individuales)</title>
 </head>
 <body>
-    <img src="assets/images/logo-ipduv.png" style="max-width: 350px;">
+    <img src="assets/images/logo-ipduv.png" style="max-width: 250px;">
 <br>
 <table style="width:100%; background-image: url('assets/images/fondo.jpg')  ">
         <tr>
